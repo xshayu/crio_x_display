8136a716fb1d214c964aca0bea70f0185c3b887f
Ayush Wardhan (throwaway) <45749740+xshayu@users.noreply.github.com>
2024-12-12 07:43:29 +0000

    [FEAT] Implemented assignment

---
+++ x_display/src/App.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [name, setName] = useState<string>();
+
+  const handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const firstName = formData.get('firstName') as string;
+    const lastName = formData.get('lastName') as string;
+    setName(`${firstName} ${lastName}`);
+  }
+
+  return (
+    <>
+    <h1>Full Name Display</h1>
+    <form
+      onSubmit={handleFormSubmit}
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'start',
+        gap: '5px'
+      }}  
+    >
+      <label>
+        First Name: <input name="firstName" required/>
+      </label>
+      <label>
+        Last Name: <input name="lastName" required/>
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+    {name?.length && 
+      <p>Full Name: {name}</p>
+    }
+    </>
+  )
+}
+
+export default App
